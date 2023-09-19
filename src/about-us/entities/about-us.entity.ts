9b6bf0e5f2c246be73093196b3f4779aab1ea7dd
itsalimanuel <itskhalofuali@gmail.com>
2023-09-19 22:32:22 +0400

    homepage: model

--- src/about-us/entities/about-us.entity.ts
+++ src/about-us/entities/about-us.entity.ts
@@ -1,44 +1,39 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import mongoose, { HydratedDocument } from 'mongoose';
+import { HydratedDocument } from 'mongoose';
 
 export type AboutUsDocument = HydratedDocument<AboutUs>;
 
-@Schema({versionKey:false})
+@Schema({ versionKey: false })
 export class AboutUs {
-    @Prop()
-    title: string;
+  @Prop()
+  title: string;
 
-    @Prop()
-    description?: string;
+  @Prop()
+  description?: string;
 
-    @Prop()
-    image: string;
+  @Prop()
+  image: string;
 
-    @Prop()
-    imageTitle:  string
+  @Prop()
+  imageTitle: string;
 
-    @Prop({type:()=>[Object]})
-    list: any[]
+  @Prop({ type: () => [Object] })
+  list: any[];
 
-    @Prop()
-    ar_title: string;
+  @Prop()
+  ar_title: string;
 
-    @Prop()
-    ar_description?: string;
+  @Prop()
+  ar_description?: string;
 
-    @Prop()
-    ar_image: string;
+  @Prop()
+  ar_image: string;
 
-    @Prop()
-    ar_imageTitle:  string
+  @Prop()
+  ar_imageTitle: string;
 
-    @Prop({type:()=>[Object]})
-    ar_list: any[]
-
-   
+  @Prop({ type: () => [Object] })
+  ar_list: any[];
 }
-
-
-
 
 export const AboutUsSchema = SchemaFactory.createForClass(AboutUs);
---
+++ src/homepage/dto/create-homepage.dto.ts
@@ -0,0 +1,70 @@
+import { ApiProperty } from "@nestjs/swagger";
+
+export class CreateHomepageDto {
+  @ApiProperty()
+  introTitle: string;
+
+  @ApiProperty()
+  ar_introTitle: string;
+
+  @ApiProperty()
+  introDescription: string;
+
+  @ApiProperty()
+  ar_introDescription: string;
+
+  @ApiProperty()
+  list: any[];
+
+  @ApiProperty()
+  ar_list: any[];
+
+  @ApiProperty()
+  schoolTitle: string;
+
+  @ApiProperty()
+  ar_schoolTitle: string;
+
+  @ApiProperty()
+  schoolDescription: string;
+
+  @ApiProperty()
+  ar_schoolDescription: string;
+
+  @ApiProperty()
+  serviceTitle: string;
+
+  @ApiProperty()
+  ar_serviceTitle: string;
+
+  @ApiProperty()
+  serviceDescription: string;
+
+  @ApiProperty()
+  ar_serviceDescription: string;
+
+  @ApiProperty()
+  eventTitle: string;
+
+  @ApiProperty()
+  ar_eventTitle: string;
+
+  @ApiProperty()
+  eventDescription: string;
+
+  @ApiProperty()
+  ar_eventDescription: string;
+
+  @ApiProperty()
+  policeTitle: string;
+
+  @ApiProperty()
+  ar_policeTitle: string;
+
+  @ApiProperty()
+  policeDescription: string;
+
+  @ApiProperty()
+  ar_policeDescription: string;
+}
+
