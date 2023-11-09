ea793f3dab0561fb02667511a0b96546d6cb4172
itsalimanuel <itskhaloufali@gmail.com>
2023-11-09 21:52:49 +0400

    merge: fix

--- src/commander/dto/update-commander.dto.ts
+++ src/commander/dto/update-commander.dto.ts
@@ -1,4 +1,4 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateCommanderDto } from './create-commander.dto';
-
+// just to push
 export class UpdateCommanderDto extends PartialType(CreateCommanderDto) {}
