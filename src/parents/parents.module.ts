643dcf72cdd1821b7db5963f6abd5ffc19d7c39e
itsalimanuel <itskhaloufali@gmail.com>
2023-11-26 20:26:52 +0400

    (fix): module

--- src/parents/parents.module.ts
+++ src/parents/parents.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common';
 import { ParentsService } from './parents.service';
 import { ParentsController } from './parents.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { Parent } from 'src/parents/entities/parent.entity';
-import { PartnerSchema } from 'src/partners/entities/partner.entity';
+import { Parent, ParentSchema } from 'src/parents/entities/parent.entity';
+
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Parent.name, schema: PartnerSchema }]),
+    MongooseModule.forFeature([{ name: Parent.name, schema: ParentSchema }]),
   ],
   controllers: [ParentsController],
   providers: [ParentsService],
