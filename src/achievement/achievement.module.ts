d5e16ef52e1c547b4353df23cba27acccf89549a
itsalimanuel <itskhaloufali@gmail.com>
2023-12-16 13:00:37 +0400

    (hotfix): award/achieve

--- src/achievement/achievement.module.ts
+++ src/achievement/achievement.module.ts
@@ -2,14 +2,16 @@ import { Module } from '@nestjs/common';
 import { AchievementService } from './achievement.service';
 import { AchievementController } from './achievement.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AchievementSchema } from 'src/achievement/entities/achievement.entity';
-import { Award } from 'src/awards/entities/award.entity';
+import {
+  Achievement,
+  AchievementSchema,
+} from 'src/achievement/entities/achievement.entity';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       {
-        name: Award.name,
+        name: Achievement.name,
         schema: AchievementSchema,
       },
     ]),
