e3b069ae404c438ff7fe7902b2899f4b2aa3bd4e
mauwia <mauwia.atif@gmail.com>
2024-03-16 14:11:22 +0300

    fix:payload too Large issue

--- src/main.ts
+++ src/main.ts
@@ -37,8 +37,8 @@ import * as bodyParser from 'body-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.use(bodyParser.json({ limit: '10mb' }));
-  app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
   //Swagger Configuration
   const config = new DocumentBuilder()
     .setTitle('Chat App')
